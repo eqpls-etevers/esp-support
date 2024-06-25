4aa6eaf3818ca6d50fee7015e0a2f9389e66ab24
jzide <jzide@equal.jz>
2024-06-25 22:40:50 +0900

    update

--- server/service/routes.py
+++ server/service/routes.py
@@ -12,7 +12,10 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, Background
 from common import getConfig, Logger, MultiTask, AsyncRest
 from .controls import Control
 from common import ID, ModelStatus
-from schema.sample.model import Message
+
+from schema.sample.model import Blog, Message
+import httpx
+import aiohttp
 
 #===============================================================================
 # SingleTone
@@ -25,59 +28,21 @@ ctrl = Control(api, config)
 #===============================================================================
 # API Interfaces
 #===============================================================================
+@api.get('/test1')
+async def test1():
+    return await Blog.searchModels()
 
+@api.get('/test2')
+async def get_httpx():
+    async with httpx.AsyncClient() as client:
+        resp = await client.get('http://localhost:8081/uerp/v1/sample/model/blog')
+        # print(resp.status_code)
+        # print(resp.text)
+    return {}
 
-@api.get('/message', tags=['Message'])
-async def get_message(request:Request) -> list[Message]:
-    query = request.query_params._dict
-    return [message for message in ctrl.messageMap.values()]
+@api.get('/test3')
+async def get_aiohttp():
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), raise_for_status=True) as session:
+        resp = await session.get('http://localhost:8081/uerp/v1/sample/model/blog')
+    return {}
 
-
-@api.post('/message', tags=['Message'])
-async def create_message(message:Message) -> Message:
-    message = message.setID(path='', type=type(message)).updateStatus()
-    ctrl.messageMap[message.id] = message
-    return message
-
-
-@api.put('/message/{id}', tags=['Message'])
-async def update_message(id:ID, message:Message) -> Message:
-    message = message.setID(path='', type=type(message), id=id).updateStatus()
-    ctrl.messageMap[message.id] = message
-    return message
-
-
-@api.delete('/message/{id}', tags=['Message'])
-async def delete_message(id:ID) -> ModelStatus:
-    ctrl.messageMap.pop(id)
-    return ModelStatus(id=id, type='unknown', ref='', status='deleted')
-
-
-@api.get('/multitask', tags=['Task'])
-async def run_multitask() -> List[Any]:
-    async with AsyncRest('https://google.co.kr') as rest:
-        async with MultiTask() as multi:
-            for _ in range(0, 10): multi(rest.get('/'))
-            return await multi.wait()
-
-
-async def backgroundTask(text):
-    LOG.INFO(text)
-    
-
-@api.get('/background', tags=['Task'])
-async def run_background(
-        background:BackgroundTasks,
-        text:Annotated[Literal['hahaha', 'hohoho', 'huhuhu'] | None, Query(alias='$text', description='laugh')]=None
-    ):
-    background.add_task(backgroundTask, text)
-
-
-@api.websocket('/websocket', tags=['WebSocket'])
-async def websocket(socket:WebSocket):
-    await socket.accept()
-    while True:
-        try:
-            payload = await socket.receive_json()
-            await socket.send_json(payload)
-        except WebSocketDisconnect: break
